4fce91b253f01cd079099e1a4884170705e78b59
Jyoti Kumari <jkgupta273@gmail.com>
2022-08-13 23:23:31 +0530

    13th Aug MongooesDB Assingment of POST-GET API

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Jyoti273-db:djukOqR9QbI5Itvc@cluster0.nzuylps.mongodb.net/Jyoti273-db", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,13 +2,18 @@ const express = require('express');
 const router = express.Router();
 const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
+const bookInfo = require("../controllers/bookController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
 router.post("/createUser", UserController.createUser  )
-
 router.get("/getUsersData", UserController.getUsersData)
+
+// Book Schema
+router.post("/createNewBook", bookInfo.createBookData)
+router.get("/getBookList", bookInfo.getBookData)
+
 
 module.exports = router;
