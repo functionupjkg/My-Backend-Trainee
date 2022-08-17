8b52f5878809be69ce8c71743bdaaf98c118a08d
Jyoti Kumari <jkgupta273@gmail.com>
2022-08-17 19:03:08 +0530

    assingment 16 augth

--- src/routes/route.js
+++ src/routes/route.js
@@ -9,11 +9,16 @@ router.get("/test-me", function (req, res) {
 })
 
 router.post("/createUser", UserController.createUser  )
-
 router.get("/getUsersData", UserController.getUsersData)
 
-router.post("/createBook", BookController.createBook  )
 
-router.get("/getBooksData", BookController.getBooksData)
+// This is our Book Schema Assignment on 16th Aug. 22 =============
+router.post("/createBook", BookController.createBook  )
+router.get("/getBooksData", BookController.getBook)
+router.get("/getBook", BookController.getBook)
+router.post("/getBooksInYear", BookController.bookInYear)
+router.get("/getParticularBooks", BookController.particularBooks)
+router.get("/getXINRBooks", BookController.getINRBooks)
+router.get("/getRandomBooks", BookController.getRandomBooks)
 
 module.exports = router;
