61241982d1123f1225a35759d8228efd709e32fc
Jyoti Kumari <jkgupta273@gmail.com>
2022-08-18 09:35:07 +0530

    17-Aug Assingnment Book-Author Schema

---
+++ src/controllers/bookController.js
@@ -0,0 +1,99 @@
+const { count } = require("console")
+const BooksModel= require("../models/booksModel")
+const AuthorsModel = require("../models/authorsModel")
+// =================================== to create authors database ===========================================================================================
+const createAuthors= async function (req,res) {
+    let data = req.body
+
+    let savedData = await AuthorsModel.create(data)
+    res.send({msg: savedData})
+}
+//===================================to create book library =============================================================================
+const createBooks= async function (req, res) {
+    let data= req.body
+
+    let savedData= await BooksModel.create(data)
+    res.send({msg: savedData})
+}
+// =========================================to getBookdata  whose author is chetan bhagat or id is 1 ======================================================================================
+const getBooksData= async function (req,res) {
+    let allBooks= await AuthorsModel.findOne({author_name : "Chetan Bhagat"}).select({author_id: 1, _id:0})
+    console.log(allBooks);
+    let list = await BooksModel.find(allBooks)
+    res.send({msg:list})
+}
+//=====================to get author name whose name two state and change their price 100 and after show =======================================================================================
+const getAuthorsName= async function (req,res){
+    let allAuthors = await BooksModel.findOneAndUpdate({name : "Two states"} ,{$set : {price: 100}},{new:true})
+    console.log(allAuthors);
+    let getAuthors = await AuthorsModel.findOne({author_id: allAuthors.author_id}).select({author_name: 1, _id:0})
+    res.send({msg: allAuthors, author_name : getAuthors.author_name})
+    // let author = await AuthorsModel.find()
+
+}
+
+//=========================to get authour by price whose book price 50 and 100 and whose authour id 1 ===========================================================================================================
+
+const getAuthorsByPrice = async function (req,res){
+    let allAuthors = await BooksModel.find({price:{$gte:50,$lte:100}}).select({author_id:1,_id:0});
+    let result = [];
+    for(let i = 0; i<allAuthors.length; i++){
+        let output = await AuthorsModel.findOne(allAuthors[i]).select({author_name:1,_id:0});
+        result.push(output.author_name)
+    } 
+    res.send({msg: result});
+}
+//======================================================================================================================================
+// const getBooksData= async function (req, res) {
+//     let allBooks= await BooksModel.find( {authorName : "HO" } )
+//     console.log(allBooks)
+//     if (allBooks.length > 0 )  res.send({msg: allBooks, condition: true})
+//     else res.send({msg: "No books found" , condition: false})
+// }
+
+
+const updateBooks= async function (req, res) {
+    let data = req.body // {sales: "1200"}
+    // let allBooks= await BookModel.updateMany( 
+    //     { author: "SK"} , //condition
+    //     { $set: data } //update in data
+    //  )
+    let allBooks= await BookModel.findOneAndUpdate( 
+        { authorName: "ABC"} , //condition
+        { $set: data }, //update in data
+        { new: true , upsert: true} ,// new: true - will give you back the updated document // Upsert: it finds and updates the document but if the doc is not found(i.e it does not exist) then it creates a new document i.e UPdate Or inSERT  
+     )
+     
+     res.send( { msg: allBooks})
+}
+
+const deleteBooks= async function (req, res) {
+    // let data = req.body 
+    let allBooks= await BookModel.updateMany( 
+        { authorName: "FI"} , //condition
+        { $set: {isDeleted: true} }, //update in data
+        { new: true } ,
+     )
+     
+     res.send( { msg: allBooks})
+}
+
+
+
+
+// CRUD OPERATIONS:
+// CREATE
+// READ
+// UPDATE
+// DELETE
+
+
+
+
+module.exports.getAuthorsByPrice =getAuthorsByPrice 
+module.exports.getAuthorsName=getAuthorsName
+module.exports.createAuthors=createAuthors
+module.exports.createBooks= createBooks 
+module.exports.getBooksData= getBooksData
+module.exports.updateBooks= updateBooks
+module.exports.deleteBooks= deleteBooks
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,53 @@
 const express = require('express');
 const router = express.Router();
-const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
-const bookInfo = require("../controllers/bookController")
+// const UserModel= require("../models/userModel.js")
+// const UserController= require("../controllers/userController")
+const BookController= require("../controllers/bookController")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+// router.get("/test-me", function (req, res) {
+//     res.send("My first ever api!")
+// })
 
-router.post("/createUser", UserController.createUser  )
-router.get("/getUsersData", UserController.getUsersData)
+// ====================================================================================================================
 
-// Book Schema
-router.post("/createNewBook", bookInfo.createBookData)
-router.get("/getBookList", bookInfo.getBookData)
+router.post("/createAuthors", BookController.createAuthors)
+router.post("/createBooks", BookController.createBooks  )
 
+//=====================================================================================================================
+
+router.get("/getBooksData", BookController.getBooksData)
+//=====================================================================================================================
+
+router.get("/getAuthorsName", BookController.getAuthorsName)
+//=======================================================================================================================
+router.get("/getAuthorsByPrice", BookController.getAuthorsByPrice )
+
+//=======================================================================================================================
+// router.post("/createUser", UserController.createUser  )
+
+// router.get("/getUsersData", UserController.getUsersData)
+
+
+// router.post("/updateBooks", BookController.updateBooks)
+// router.post("/deleteBooks", BookController.deleteBooks)
+
+//MOMENT JS
+// const moment = require('moment');
+// router.get("/dateManipulations", function (req, res) {
+    
+    // const today = moment();
+    // let x= today.add(10, "days")
+
+    // let validOrNot= moment("29-02-1991", "DD-MM-YYYY").isValid()
+    // console.log(validOrNot)
+    
+//     const dateA = moment('01-01-1900', 'DD-MM-YYYY');
+//     const dateB = moment('01-01-2000', 'DD-MM-YYYY');
+
+//     let x= dateB.diff(dateA, "days")
+//     console.log(x)
+
+//     res.send({ msg: "all good"})
+// })
 
 module.exports = router;
